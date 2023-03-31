8cfd338990591b06e6f6ea7d973d0a166a392ee4
_diachuk_ <dyachukpasha605@gmail.com>
2023-03-31 14:45:22 +0300

    lesson 155. PropTypes

--- marvel_starter/src/components/charInfo/CharInfo.js
+++ marvel_starter/src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import PropTypes from 'prop-types';
 
 import MarvelService from "../../services/MarvalService";
 import Spinner from "../spinner/spinner";
@@ -132,5 +133,8 @@ const View = ({char}) => {
 
 }
 
+CharInfo.propTypes = {
+    charId: PropTypes.number
+}
 
 export default CharInfo;
